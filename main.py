11e3eb723d7dbbf1798bdc7cd77a5b0c42b5ba42
Will Ondrik <wondrik@my.bcit.ca>
2024-06-18 12:43:05 -0700

    Modified comment structure for the main function

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import format_data # Extracts and formats the new DPP Data
 import merge_data # Merges new DPP workorders to the master sheet
 
-# The main function will call the format_data and merge_data functions
-# This will format the new DPP workorders and append them to the master sheet
-# A temporary copy of the master sheet is created in the merge function
-    # It will be deleted if the merge is successful
+"""
+The main function will call the format_data and merge_data functions
+    - This will format the new DPP workorders and append them to the master sheet
+    - A temporary copy of the master sheet is created in the merge function
+    - It will be deleted if the merge is successful
+"""
 def main(master_sheet_filepath, raw_dpp_filepath):
 
     print('Executing script...')
@@ -14,7 +16,7 @@ def main(master_sheet_filepath, raw_dpp_filepath):
     formatted_dpp_filepath = format_data.format_workorders(raw_dpp_filepath)
 
     print('Formatting complete. Merging data...')
-    
+
     # Merge the formatted DPP data into the master sheet
     merge_data.merge(master_sheet_filepath, formatted_dpp_filepath)
 
